6f97bc43427ef7db489fbde40de08134630d89fd
Farhan Tariq <farhantariq12b@gmail.com>
2023-08-21 17:31:14 +0500

    adds correct logic to update settings

--- routes/settings.js
+++ routes/settings.js
@@ -1,84 +1,13 @@
 var express = require("express");
 var router = express.Router();
-const { validate } = require("../util/requestValidate");
 const settings = require("../service/settings/sellerSettings");
-const constant = require("../util/constant");
 var _ = require("lodash");
 
-router.post("/update", (req, res, next) => {
-	let setting = req.body;
-	validate(setting, ["SellerId", "MWSAuthToken", "AWSAccessKeyId", "ClientSecret", "MarketPalaceID",], res);
-	settings.getSetting(req.loggedUser.username, constant.SELLERSETTINGGROUP)
-		.then((result) => {
-			if (result && result.length > 0) {
-				settings.updateSetting(
-					req.loggedUser.username,
-					setting,
-					constant.SELLERSETTINGGROUP
-				).then(responce => {
-					res.end("Updated Successfully")
-				})
-					.catch(error => {
-						res.status(500).send(error);
-					})
-			} else {
-				settings.addSetting(
-					req.loggedUser.username,
-					setting,
-					constant.SELLERSETTINGGROUP
-				).then(responce => {
-					res.end("Updated Successfully")
-				})
-					.catch(error => {
-						res.status(500).send(error);
-					})
-			}
-		});
-});
-router.get("/getsetting", (req, res, next) => {
+router.get("/getsetting",settings.getSetting);
 
-	settings.getSetting(req.loggedUser.username, req.query.settinggroup)
-		.then(result => {
-			if (result && result.length > 0) {
-				res.send(result[0]);
-			}
-			else {
-				res.status(404);
-			}
-		})
-		.catch(error => {
-			res.status(500).send(error);
-		})
+router.post("/updatesettings",settings.addOrUpdateSetting);
 
-});
-router.post("/updatesettings", (req, res, next) => {
-	let setting = req.body;
+router.post("/update", settings.addOrUpdateSetting);
 
-	settings.getSetting(req.loggedUser.username, setting.settinggroup)
-		.then((result) => {
-			if (result && result.length > 0) {
-				settings.updateSetting(
-					req.loggedUser.username,
-					setting,
-					setting.settinggroup
-				).then(responce => {
-					res.end("Updated Successfully")
-				})
-					.catch(error => {
-						res.status(500).send(error);
-					})
-			} else {
-				settings.addSetting(
-					req.loggedUser.username,
-					setting,
-					setting.settinggroup
-				).then(responce => {
-					res.end("Updated Successfully")
-				})
-					.catch(error => {
-						res.status(500).send(error);
-					})
-			}
-		});
-});
+
 module.exports = router;
--- service/settings/sellerSettings.js
+++ service/settings/sellerSettings.js
@@ -1,47 +1,86 @@
-const mysql = require('../../service/mysql'),
-    { updateSetting, addSetting, getSetting } = require('../../util/sqlquery'),
-    format = require('string-format');
+const mysql = require("../../service/mysql"),
+  { updateSetting, addSetting, getSetting } = require("../../util/sqlquery"),
+  format = require("string-format");
 const constant = require("../../util/constant");
+const { validate } = require('../../util/requestValidate' );
 
 class sellerSettings {
-    getSetting = function (userName, group) {
-        return new Promise((resolve, reject) => {
-            mysql.query(getSetting, [group])
-                .then(settings => resolve(settings))
-                .catch(err => reject(err));
-        })
+  async getSetting(req, res, next) {
+    const { settinggroup } = req.query;
+    try {
+      const result = await mysql.query(getSetting, [
+        settinggroup,
+        req.loggedUser.username,
+      ]);
+      if (result?.length) {
+        return res.status(200).send(result[0]);
+      } else {
+        return res.status(404).send();
+      }
+    } catch (err) {
+      res.status(500).send(err);
     }
-    updateSetting = function (username, setting, group) {
-        return new Promise((resolve, reject) => {
-            mysql.query(updateSetting, [JSON.stringify(setting), group])
-                .then(settings => resolve(settings))
-                .catch(err => reject(err));
-        })
+  }
+  async addOrUpdateSetting(req, res, next) {
+    const setting = req.body;
+    //validating feilds if settinggroup is 'sellerSettingGroup'
+    if (setting.settinggroup === "sellerSettingGroup") {
+      validate(
+        setting,
+        [
+          "SellerId",
+          "MWSAuthToken",
+          "AWSAccessKeyId",
+          "ClientSecret",
+          "MarketPalaceID",
+        ],
+        res
+      );
+      setting["settinggroup"] = constant.SELLERSETTINGGROUP;
     }
-    addSetting = function (username, setting, group) {
-        var newSetting = {
-            settings: JSON.stringify(setting),
-            username: username,
-            settinggroup: group
-        }
-        return new Promise((resolve, reject) => {
-            mysql.query(addSetting, newSetting)
-                .then(settings => resolve(settings))
-                .catch(err => reject(err));
-        })
+    try {
+      //checking for existing setting.
+      const result = await mysql.query(getSetting, [
+        setting["settinggroup"],
+        req.loggedUser.username,
+      ]);
+      if (result.length) {
+        //update setting if exists.
+        await mysql.query(updateSetting, [
+          JSON.stringify(setting),
+          setting["settinggroup"],
+          req.loggedUser.username,
+        ]);
+        return res.status(200).send("Updated successfully.");
+      } else {
+        //add new setting
+        const newSetting = {
+          settings: JSON.stringify(setting),
+          username: req.loggedUser.username,
+          settinggroup: constant.SELLERSETTINGGROUP,
+        };
+        await mysql.query(addSetting, newSetting);
+        return res.status(200).send("Setting added successfully.");
+      }
+    } catch (err) {
+      console.error(err);
+      return res.status(500).send(err);
     }
+  }
 
-    async getSellerId(user) {
-        var config = await this.getSetting(user, constant.SELLERSETTINGGROUP);
-        return new Promise((resolve, reject) => {
-            if (config && config.length > 0 && config[0].settings) {
-                resolve(JSON.parse(config[0].settings));
-            }
-            else {
-                reject();
-            }
-        }
-        )
-    }
+  async getSellerId(user) {
+    return new Promise((resolve, reject) => {
+      mysql
+        .query(getSetting, [constant.SELLERSETTINGGROUP, user])
+        .then((settings) => {
+          if (settings.length && settings[0].settings) {
+            resolve(JSON.parse(settings[0].settings));
+          } else {
+            reject();
+          }
+        })
+        .catch((err) => reject());
+    });
+  }
 }
-module.exports = new sellerSettings;
+module.exports = new sellerSettings();
